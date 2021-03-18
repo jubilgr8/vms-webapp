e1bc7aec36b443f90d830b2d2ad38080b56fcfa7
jubilgr8 <johna177@gmail.com>
2021-03-18 17:14:21 +0530

    Dynammic Dropdown List

--- apps/vms-web/src/app/layout/navbar/navbar.component.ts
+++ apps/vms-web/src/app/layout/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { User } from 'libs/api/src';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -12,10 +12,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./navbar.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  ngOnInit(): void {
+  }
   @Input() user: User;
-  @Input() isLoggedIn: boolean;
-
+  @Input() isCollapse: boolean;
+  public isCollapsed: boolean = true;
+  public isCollapsedv: boolean = true;
   OnMenuClick(event: Event){
     event.stopPropagation();
     
